35659b7772f78a2fac41c09db28fac2bb8eb43c9
Detims <2005.nnhan@gmail.com>
2025-03-31 14:09:17 -0700

    router

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return(
+        <section>
+            <h1>Goodbye World</h1>
+        </section>
+    )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <section id='home'>
+            <h1>Hello World</h1>
+        </section>
+    )
+}
+
+export default Home
