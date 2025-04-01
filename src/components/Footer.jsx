16d9e855e4c082be9c024143fadc90b62932b552
Detims <2005.nnhan@gmail.com>
2025-03-31 20:57:56 -0700

    make navbar and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+const Footer = () => {
+    return(
+        <footer>
+            <h2>Footer</h2>
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 'use client'
 
 const Navbar = () => {
-
+    <nav className="fixed top-0">
+        <div className="flex mx-auto">
+            <ul className="hidden md:flex items-center">
+            </ul>
+        </div>
+    </nav>
 }
 
 export default Navbar
