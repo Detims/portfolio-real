4bd7578deb1aa8d41e12f51dc51781351b379227
Detims <2005.nnhan@gmail.com>
2025-04-03 18:06:12 -0700

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,15 +3,15 @@ import { motion } from "framer-motion";
 
 const Main = () => {
   return (
-    <div className="relative flex flex-col overflow-hidden">
+    <div className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden">
       {/* Content */}
       <div className="text-center">
-        <h1>Hi, I'm Nhan Nguyen</h1>
+        <h1 className="mb-6 text-5xl">Hi, I'm Nhan Nguyen</h1>
 
         {/* Subheading */}
-        <p>CS Student | Aspiring Software Engineer</p>
+        <p className="mb-2">CS Student | Aspiring Software Engineer</p>
 
-        <div className="flex flex-col md:flex-row items-center justify-center"> 
+        <div className="flex flex-col md:flex-row items-center justify-center gap-2"> 
           <a href="/projects" className="px-6 py-2 bg-gray-300 rounded-lg text-black">
             Projects
           </a>
