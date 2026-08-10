9c2c65080e350b23a40826db575e60ae2a6541cb
Nhan Nguyen <2005.nnhan@gmail.com>
2026-08-09 21:41:32 -0700

    Implement tailwind into config and site

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router/dom';
 import { router } from './router.tsx';
-import './index.css';
+import './global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
