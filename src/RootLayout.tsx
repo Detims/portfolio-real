39c24d213d6f2f2393d748dea329f420706335da
Nhan Nguyen <2005.nnhan@gmail.com>
2026-08-09 20:45:44 -0700

    Fix erroneous formatting in root layout

--- src/RootLayout.tsx
+++ src/RootLayout.tsx
@@ -3,16 +3,11 @@ import { Sidebar } from "./components/sidebar";
 
 export function RootLayout() {
     return (
-        <div
-            lang="en"
-            className="h-full antialiased"
-        >
-            <body className="min-h-full flex flex-col bg-black text-white">
-                <Sidebar />
-                <main>
-                    <Outlet />
-                </main>
-            </body>
+        <div className="min-h-full flex flex-col bg-black text-white">
+            <Sidebar />
+            <main>
+                <Outlet />
+            </main>
         </div>
     )
 }
