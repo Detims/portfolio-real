9223fa6be4735b6101ae3e10a456289b5f36df6c
Nhan Nguyen <2005.nnhan@gmail.com>
2026-08-12 18:03:03 -0700

    Add profile image and description

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -13,7 +13,7 @@ export function Divider({ id, label, children }: DividerProps) {
             className="mx-6 my-24 md:ml-56 md:mr-12 lg:ml-64 lg:mr-20"
         >
             <header className="mb-16 pb-4 border-b-2 border-white/15">
-                <h2 className="ml-6 text-lg uppercase tracking-widest text-white/80">
+                <h2 className="ml-6 text-lg md:text-xl uppercase tracking-widest text-white/80">
                     {label}
                 </h2>
             </header>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,8 +4,23 @@ export function About() {
     return(
         <>
             <Divider label="About" id="about">
-                <div className="flex flex-col items-center justify-center">
-                    
+                <div className="flex flex-col lg:flex-row gap-12 items-center justify-center">
+                    <div className="mx-auto w-64 shrink-0 md:mx-0 md:w-80">
+                        <div className="relative aspect-3/4 overflow-hidden rounded-md">
+                            <img 
+                                src="/images/profile.jpg"
+                                alt="Nhan Nguyen"
+                                className=""
+                            /> 
+                        </div>
+                    </div>
+                    <p className="text-xl mx-12 md:mx-0 leading-relaxed text-white/85">
+                        I'm a Computer Science student at California State University, Long Beach, and also
+                        pursuing a minor in Statistics. I have a deep passion for artificial intelligence and 
+                        machine learning, with a focus on tackling real-world problems with data-driven solutions.
+                        I'm always eager to build my skills and connect with likewise passionate individuals to grow 
+                        as a developer and person.
+                    </p>
                 </div>
             </Divider>
         </>
