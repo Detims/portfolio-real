30f83da91df6eee753032b9a5fcb220a5c6ce6c5
Nhan Nguyen <2005.nnhan@gmail.com>
2026-08-21 15:50:38 -0700

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router/dom';
 import { router } from './router.tsx';
+import { Analytics } from '@vercel/analytics/react';
 import './global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <RouterProvider router={router}/>
   </StrictMode>,
 );
